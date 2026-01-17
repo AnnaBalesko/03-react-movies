5ec9a111175644513f2799b77bf5e52446041a4f
Anna Balesko <babasheldona@icloud.com>
2026-01-17 19:34:50 +0100

    add components

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,60 @@
-import SearchBar from '../SearchBar/SearchBar';
-// import css from './App.module.css';
+import { useState } from "react";
+import SearchBar from "../SearchBar/SearchBar";
+import css from "./App.module.css";
+import type { Movie } from "../../types/movie";
+import { fetchMovies } from "../../services/movieService";
+import toast, { Toaster } from "react-hot-toast";
+import MovieGrid from "../MovieGrid/MovieGrid";
+import Loader from "../Loader/Loader";
+import ErrorMessage from "../ErrorMessage/ErrorMessage";
+import MovieModal from "../MovieModal/MovieModal";
 
-export default function App(){
-    return (
-<SearchBar/>
-    )
+export default function App() {
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+    setSelectedMovie(null);
+  };
+
+  const handleSelectMovie = (movie: Movie) => {
+    setSelectedMovie(movie);
+    setIsModalOpen(true);
+  };
+
+  const handleSearch = async (query: string) => {
+    try {
+      setMovies([]);
+      setIsLoading(true);
+      setIsError(false);
+      const data = await fetchMovies(query);
+      if (data.length === 0) {
+        toast.error("No movies found for your request.");
+        return;
+      }
+      setMovies(data);
+    } catch {
+      setIsError(true);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+  return (
+    <div className={css.app}>
+      <SearchBar onSubmit={handleSearch} />
+      {isModalOpen && selectedMovie && (
+        <MovieModal movie={selectedMovie} onClose={closeModal} />
+      )}
+      <Toaster position="top-center" reverseOrder={false} />
+      {movies.length > 0 && (
+        <MovieGrid onSelect={handleSelectMovie} movies={movies} />
+      )}
+      {isLoading && <Loader />}
+      {isError && <ErrorMessage />}
+    </div>
+  );
 }
-
---
+++ src/services/movieService.ts
@@ -0,0 +1,21 @@
+import axios from "axios";
+import type { Movie } from "../types/movie";
+interface MoviesRespons {
+  results: Movie[];
+}
+
+export const fetchMovies = async (query: string): Promise<Movie[]> => {
+  const config = {
+    params: {
+      query,
+    },
+    headers: {
+      Authorization: `Bearer ${import.meta.env.VITE_TMDB_TOKEN}`,
+    },
+  };
+  const response = await axios.get<MoviesRespons>(
+    "https://api.themoviedb.org/3/search/movie",
+    config,
+  );
+    return response.data.results;
+};
